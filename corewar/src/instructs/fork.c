601592679592384da73cb14f3043d75d520dbcbd
Kerosene2000 <renault.cle@gmail.com>
2015-07-31 00:32:37 +0200

    fork done(check it), lld todo

---
+++ corewar/src/instructs/fork.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fork.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: crenault <crenault@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/07/31 00:28:23 by crenault          #+#    #+#             */
+/*   Updated: 2015/07/31 00:28:23 by crenault         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "vm_protos.h"
+#include "libft.h"
+
+// Pas d’octet de codage des paramètres, prend un index, opcode 0x0c. Crée
+// un nouveau processus, qui hérite des différents états de son père,
+// à part son PC, qui est mis à (PC + (1er paramètre % IDX_MOD)).
+void	fork_(t_vm *vm, t_process *process)
+{
+	size_t		pc;
+	t_process	*new;
+
+	new = dup_process(process);
+	new->nb_lives = 0; // TODO check if keep that
+	pc = loadmem(&vm->arena, sizeof(int), process->instruction.params[0]);
+	move_pc(&new->pc, pc % IDX_MOD);
+	ft_lstadd(&vm->processes, ft_lstcreate(new, sizeof(t_process)));
+}
---
+++ corewar/src/instructs/lld.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lld.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: crenault <crenault@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/07/31 00:32:08 by crenault          #+#    #+#             */
+/*   Updated: 2015/07/31 00:32:08 by crenault         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "vm_protos.h"
+
+// Signifie long-load, donc son opcode est évidemment 13. C’est la même chose
+// que ld, mais sans % IDX_MOD. Modifie le carry.
+void		lld(t_vm *vm, t_process *process)
+{
+	// TODO
+	(void)vm;
+	(void)process;
+}
