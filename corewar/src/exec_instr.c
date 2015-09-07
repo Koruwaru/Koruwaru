860cb508ef5835e7b7f14f6c101d69ba294deedd
Tom MIELCZAREK <tmielcza@e2r7p13.42.fr>
2015-09-07 22:29:39 +0200

    proc 13 does some shit at cycle 8909 (helltrain). Fix it.

--- corewar/src/exec_instr.c
+++ corewar/src/exec_instr.c
@@ -6,7 +6,7 @@
 /*   By: tmielcza <tmielcza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/09/02 15:02:59 by tmielcza          #+#    #+#             */
-/*   Updated: 2015/09/07 18:49:31 by tmielcza         ###   ########.fr       */
+/*   Updated: 2015/09/07 22:21:02 by tmielcza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,9 +26,10 @@ void		exec_instr(t_vm *vm, t_process *proc)
 	t_op *op = get_op(proc->instruction.opcode);
 	char *name = op ? op->name : "null";
 	t_instruction *i = &proc->instruction;
-//	printf("(%d) do instr %s|%d| - %d %d %d <%d> \n", proc->id, name, proc->instruction.opcode, i->params[0], i->params[1], i->params[2], proc->carry);
 	opcode = proc->instruction.opcode;
-	load_instr(proc, &vm->arena);
+	if (opcode != 0)
+		load_instr(proc, &vm->arena);
 	proc->instruction.opcode = opcode;
+	printf("(%d) do instr %s|%d| - %d %d %d <%d> [:%d/%x]\n", proc->id, name, proc->instruction.opcode, i->params[0], i->params[1], i->params[2], proc->carry, proc->pc, proc->pc);
 	tabs[(int)proc->instruction.opcode](vm, proc);
 }
