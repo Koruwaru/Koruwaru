ba115d0607925b5a3c85e0db44b55ee05362d53e
Tom MIELCZAREK <tmielcza@e2r5p10.42.fr>
2015-10-14 23:00:05 +0200

    Ze endo desu yo

--- PetitClem.s
+++ PetitClem.s
@@ -3,14 +3,6 @@
 
 code:
 	ldi %:copy, %0, r3
-	ldi %:copy, %4, r4
-	ldi %:copy, %8, r5
-	ldi %:copy, %12, r6
-	ldi %:copy, %16, r7
-	ldi %:copy, %20, r8
-	ldi %:copy, %24, r9
-	ldi %:copy, %28, r10
-	ld %120, r12
 
 start:
 	sti r1, %:live, %1
@@ -21,16 +13,34 @@ live:
 	zjmp %:start
 
 fork_start:
+	fork %:copy
 	fork %:start
+	lfork %200
+
+coucou:
+	sti r1, %:hop, %1
+
+hop:
+	live %42
+	and %0, %0, r2
+	zjmp %:hop
 
 copy:
-	sti r3, 100, %50
-	sti r4, 100, %50
-	sti r5, 100, %50
-	sti r6, 100, %50
-	sti r7, 100, %50
-	sti r8, 100, %50
-	sti r9, 100, %50
-	sti r10, 100, %50
+	sti r3, 0, %0
+	sti r3, 0, %0
+	sti r3, 0, %0
+	sti r3, 0, %0
+	sti r3, 0, %0
+	sti r3, 0, %0
+	sti r3, 0, %0
+	sti r3, 0, %0
+	sti r3, 0, %0
+	sti r3, 0, %0
+#	sti r5, 0, %20
+#	sti r6, 0, %30
+#	sti r7, 0, %40
+#	sti r8, 0, %50
+#	sti r9, 0, %60
+#	sti r10, 0, %70
 
 end:
